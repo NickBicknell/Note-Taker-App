f510d6f09293165481d1ea274e3d43667dd05152
Nick Bicknell <nicholasbicknell@gmail.com>
2023-08-17 22:50:09 -0500

    added comments describing code function

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,16 @@
+// required dependencies
 const html = require('express').Router();
 const path = require('path');
 
+// get request for the html notes page
 html.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
+// get request for all other/index html page 
 html.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// exports html router
 module.exports = html;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// required dependencies
 const express = require('express');
 const apiRoutes = require('./routes/api-routes');
 const htmlRoutes = require('./routes/html-routes');
@@ -6,10 +7,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// connection to routers
 app.use(apiRoutes);
 app.use(htmlRoutes);
 
